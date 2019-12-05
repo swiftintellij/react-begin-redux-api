837d5cec646a513ac08da6d82331aed0f4b933b2
swiftintellij <swiftintellij@gmail.com>
2019-12-06 00:42:48 +0900

    add jsonserver

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,32 +1,45 @@
 
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+// const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+//
+//
+// const posts = [
+//     {
+//         id: 1,
+//         title: "타이틀1",
+//         body: "내용1내용1내용1"
+//     },
+//     {
+//         id: 2,
+//         title: "타이틀2",
+//         body: "내용2내용2내용2"
+//     },
+//     {
+//         id: 3,
+//         title: "타이틀3",
+//         body: "내용3내용3내용3"
+//     },
+// ];
+//
+// export const getPosts = async () => {
+//     await sleep(500);
+//     return posts;
+// };
+//
+// export const getPostById = async id => {
+//     await sleep(500);
+//     return posts.find(post => post.id === id);
+//
+// };
 
 
-const posts = [
-    {
-        id: 1,
-        title: "타이틀1",
-        body: "내용1내용1내용1"
-    },
-    {
-        id: 2,
-        title: "타이틀2",
-        body: "내용2내용2내용2"
-    },
-    {
-        id: 3,
-        title: "타이틀3",
-        body: "내용3내용3내용3"
-    },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-    await sleep(500);
-    return posts;
+    const response = await axios.get("http://localhost:4000/posts");
+    return response.data;
 };
 
 export const getPostById = async id => {
-    await sleep(500);
-    return posts.find(post => post.id === id);
-
+    const response = await axios.get(`http://localhost:4000/posts/${id}`)
+    return response.data;
 };
