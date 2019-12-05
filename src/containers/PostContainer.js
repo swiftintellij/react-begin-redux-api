1111bb2615eafed88b03d42ff2d5711799b42ae2
swiftintellij <swiftintellij@gmail.com>
2019-12-05 20:08:26 +0900

    new state

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -2,19 +2,17 @@ import React, { useEffect } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {clearPost, getPost} from "../modules/posts";
 import Post from "../componets/Post";
+import {reducerUtils} from "../lib/asyncUtils";
 
 function PostContainer({postId}) {
-    const { data, loading, error } = useSelector(state => state.posts.post);
+    const { data, loading, error } = useSelector(state => state.posts.post[postId] || reducerUtils.initial());
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getPost(postId));
-        return () => {
-            dispatch(clearPost())
-        }
     }, [postId, dispatch]);
 
-    if (loading) return <div>로딩중...</div>
+    if (loading && !data) return <div>로딩중...</div>
     if (error) return <div>에러발생!</div>
     if (!data) return null;
 
